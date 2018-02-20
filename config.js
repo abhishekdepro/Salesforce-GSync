628abbb60fddefd348d33954d3ba274edfd0684f
Abhishek Dey <abhishekde@hotmail.com>
2018-02-20 21:55:55 +0530

    Added Salesforce Sync

---
+++ config.js
@@ -0,0 +1,17 @@
+/*
+Contains Config Data
+*/
+var config = {};
+
+config.SALESFORCE = {};
+config.GOOGLE = {};
+
+//salesforce settings
+config.SALESFORCE.username = 'XXX';
+config.SALESFORCE.password =  'YYY';
+
+//Google Contacts API Keys
+config.GOOGLE.apiKey = 'XXX';
+config.GOOGLE.apiSecret = 'ZZZ';
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,14 +1,54 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var bodyParser = require('body-parser'); 
+var bodyParser = require('body-parser');
+var jsforce = require('jsforce'); //used for Salesforce Connection 
+
+//global variables
+var sfAccessToken = '';
+var sfInstanceURL = '';
+
+//Environment Config Variables
+var config = require('./config');
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
-var client = require('gdata-js')('XXX', 'YYY', 'http://localhost:3000/authenticate');
 
-app.get('/', function(req, res) {
+//Google Contacts API Client auth
+var client = require('gdata-js')(config.GOOGLE.apiKey, config.GOOGLE.apiSecret, 'https://salesforce-gsync.herokuapp.com/authenticate');
+//var client = require('gdata-js')(config.GOOGLE.apiKey, config.GOOGLE.apiSecret, 'http://localhost:3000/authenticate');
+
+//Salesforce Connection using jsForce
+var conn = new jsforce.Connection({
+    loginUrl: 'https://login.salesforce.com'
+});
+
+conn.login(config.SALESFORCE.username, config.SALESFORCE.password, function (err, userInfo) {
+    if (err) { return console.error(err); }
+    // Now you can get the access token and instance URL information.
+    // Save them to establish connection next time.
+    sfAccessToken = conn.accessToken;
+    sfInstanceURL = conn.instanceUrl;
+
+    console.log(conn.accessToken);
+    console.log(conn.instanceUrl);
+    // logged in user property
+    console.log("User ID: " + userInfo.id);
+    console.log("Org ID: " + userInfo.organizationId);
+
+
+    //SOQL Queries
+    var records = [];
+    conn.query("SELECT Id, Name FROM Account ORDER BY CreatedDate ASC LIMIT 2", function (err, result) {
+        if (err) { return console.error(err); }
+        console.log("total : " + result.totalSize);
+        console.log("fetched : " + result.records[0].Name);
+    });
+});
+
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
@@ -30,32 +70,55 @@ app.get('/authenticate', function (req, res) {
             res.redirect('/getStuff');
         }
     });
-    //client.setToken({ access_token: accessToken, refresh_token: refreshToken });
-    /*client.getFeed('https://www.google.com/m8/feeds/contacts/default/full', function (err, result) {
-
-    });*/
 });
 
 app.get('/getStuff', function (req, res) {
-    client.getFeed('https://www.google.com/m8/feeds/contacts/default/full', { 'max-results': 500},
+    var contactEntryList = [];
+
+    client.getFeed('https://www.google.com/m8/feeds/contacts/default/full', { 'max-results': 5 },
         function (err, feed) {
             res.writeHead(200);
             for (var i in feed.feed.entry) {
-                if(feed.feed.entry[i].gd$phoneNumber!=undefined){
-                    var phoneNumber = ''+JSON.stringify(feed.feed.entry[i].gd$phoneNumber[0].$t).replace(/ /g,'');
+                var contactEntry = {};
+                if (feed.feed.entry[i].gd$phoneNumber != undefined) {
+                    var phoneNumber = '' + JSON.stringify(feed.feed.entry[i].gd$phoneNumber[0].$t).replace(/ /g, '');
                     console.log(phoneNumber);
-                    if(phoneNumber.startsWith("+91")){
+                    if (phoneNumber.startsWith("+91")) {
                         phoneNumber = phoneNumber.substring(3);
                         console.log(phoneNumber);
                     }
-                    res.write(phoneNumber+'\n'+JSON.stringify(feed.feed.entry[i].title.$t));
+                    res.write(phoneNumber + '\n' + JSON.stringify(feed.feed.entry[i].title.$t));
                     res.write('\n\n');
+                    contactEntry.name = JSON.stringify(feed.feed.entry[i].title.$t);
+                    contactEntry.phone = phoneNumber;
+                    contactEntryList.push(contactEntry);
                 }
             }
+            var accountList = [];
+            for(var cEntry in contactEntryList){
+                console.log('cEntry..'+JSON.stringify(contactEntryList[cEntry]));
+                var acc = {};
+                acc.Name = JSON.parse(contactEntryList[cEntry].name);
+                acc.Phone = JSON.parse(contactEntryList[cEntry].phone);
+                accountList.push(acc);
+            }
+            console.log('accountList..'+JSON.stringify(accountList));
+            //insert into Salesforce Accounts
+            conn.sobject("Account").create(accountList,
+                function (err, rets) {
+                    if (err) { return console.error(err); }
+                    for (var i = 0; i < rets.length; i++) {
+                        if (rets[i].success) {
+                            console.log("Created record id : " + rets[i].id);
+                        }
+                    }
+                    // ...
+                });
+
             res.end();
         });
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening on port 3000!');
 });
